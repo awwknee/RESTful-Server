0246b48655aec136c360bcf6f0d94fb085680926
Ani <rajani.lamichhane7@gmail.com>
2022-11-20 03:05:48 -0600

    did GET /incidents

--- server.js
+++ server.js
@@ -28,35 +28,78 @@ let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
 // GET request handler for crime codes
 app.get('/codes', (req, res) => {
     console.log(req.query); // query object (key-value pairs after the ? in the url)
-    
+
     res.status(200).type('json').send({}); // <-- you will need to change this
 });
 
 // GET request handler for neighborhoods
 app.get('/neighborhoods', (req, res) => {
     console.log(req.query); // query object (key-value pairs after the ? in the url)
-    
+
     res.status(200).type('json').send({}); // <-- you will need to change this
 });
 
 // GET request handler for crime incidents
 app.get('/incidents', (req, res) => {
-    console.log(req.query); // query object (key-value pairs after the ? in the url)
-    
-    res.status(200).type('json').send({}); // <-- you will need to change this
+
+    var statement = 'SELECT case_number, strftime("%Y-%m-%d", date_time) as date, strftime("%H:%M:%S", date_time) as time, code, incident, police_grid, neighborhood_number, block FROM Incidents ';
+ 
+    var query = Object.keys(req.query).filter(key => ['start_date', 'end_date', 'code', 'grid', 'neighborhood', 'limit'].includes(key));
+    if (!query.includes('limit')) {
+        query.push('dino')
+    }
+    query.forEach(key => {
+        switch (key) {
+            case 'start_date':
+                var start_date = req.query[key];
+                statement += `WHERE date >= (${start_date}) `;
+                break;
+            case 'end_date':
+                var end_date = req.query[key];
+                statement += `WHERE date <= (${end_date}) `;
+                break;
+            case 'code':
+                var code = req.query[key].split(',');
+                statement += `WHERE code IN (${code.join(", ")}) `;
+                break;
+            case 'grid':
+                var grid = req.query[key].split(',');
+                statement += `WHERE police_grid IN (${grid.join(", ")}) `;
+                break;
+            case 'neighborhood':
+                var neighborhood = req.query[key].split(',');
+                statement += `WHERE neighborhood_number IN (${neighborhood.join(", ")}) `;
+                break;
+            default:
+                var limit = 100;
+                if (key == 'limit') {
+                    limit = req.query[key];
+                }
+                statement += `ORDER BY date_time ASC LIMIT ${limit} `;
+                break;
+        }
+
+    });
+
+    databaseSelect(statement, {})
+    .then(rows => {
+        res.status(200).type('json').send(rows);
+    }).catch(error => {
+        res.status(401).type('json').send(error + ' you silly goose.');
+    });
 });
 
 // PUT request handler for new crime incident
 app.put('/new-incident', (req, res) => {
     console.log(req.body); // uploaded data
-    
+
     res.status(200).type('txt').send('OK'); // <-- you may need to change this
 });
 
 // DELETE request handler for new crime incident
 app.delete('/new-incident', (req, res) => {
     console.log(req.body); // uploaded data
-    
+
     res.status(200).type('txt').send('OK'); // <-- you may need to change this
 });
 
