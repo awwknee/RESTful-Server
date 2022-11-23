868403f156ae103b67911ad0238f5fd8a0c95836
Seth Zerwas <zerw2167@stthomas.edu>
2022-11-23 17:59:18 -0600

    Added code and neighborhood routes

--- server.js
+++ server.js
@@ -28,47 +28,85 @@ let db = new sqlite3.Database(db_filename, sqlite3.OPEN_READWRITE, (err) => {
 // GET request handler for crime codes
 app.get('/codes', (req, res) => {
     console.log(req.query); // query object (key-value pairs after the ? in the url)
-
-    res.status(200).type('json').send({}); // <-- you will need to change this
+    var statement = 'SELECT code, incident_type as type FROM Codes ORDER BY code ASC';
+    databaseSelect(statement, {})
+    .then(rows => {
+        res.status(200).type('json').send(rows);
+    })
+    .catch(err => {
+        res.status(401).type('json').send(err + ' you silly goose.');
+    })
 });
 
 // GET request handler for neighborhoods
 app.get('/neighborhoods', (req, res) => {
     console.log(req.query); // query object (key-value pairs after the ? in the url)
-
-    res.status(200).type('json').send({}); // <-- you will need to change this
+    var statement = 'SELECT neighborhood_number as ID, neighborhood_name as Name FROM NEIGHBORHOODS ORDER BY ID ASC';
+    databaseSelect(statement, {})
+    .then(rows => {
+        res.status(200).type('json').send(rows);
+    })
+    .catch(err => {
+        res.status(401).type('json').send(err + ' you silly goose.');
+    })
 });
 
 // GET request handler for crime incidents
 app.get('/incidents', (req, res) => {
-
+    console.log(req.query);
     var statement = 'SELECT case_number, strftime("%Y-%m-%d", date_time) as date, strftime("%H:%M:%S", date_time) as time, code, incident, police_grid, neighborhood_number, block FROM Incidents ';
- 
+    
     var query = Object.keys(req.query).filter(key => ['start_date', 'end_date', 'code', 'grid', 'neighborhood', 'limit'].includes(key));
+
     if (!query.includes('limit')) {
         query.push('limit')
     }
+
     query.forEach(key => {
         switch (key) {
             case 'start_date':
                 var start_date = req.query[key];
-                statement += `WHERE date >= (${start_date}) `;
+                statement += `WHERE date >= '${start_date}'`;
                 break;
             case 'end_date':
                 var end_date = req.query[key];
-                statement += `WHERE date <= (${end_date}) `;
+                statement += `WHERE date <= '${end_date}'`;
                 break;
             case 'code':
-                var code = req.query[key].split(',');
-                statement += `WHERE code IN (${code.join(", ")}) `;
+                var codes = req.query[key].split(',');
+                statement += `WHERE code = '${codes[0]}'`;
+                if (codes.length > 1) {
+                    for (let x=1;x<codes.length;x++) {
+                        if (x !== codes.length) {
+                            statement += ` OR `;
+                        }
+                        statement += `code = '${codes[x]}'`;
+                    }
+                }
                 break;
             case 'grid':
-                var grid = req.query[key].split(',');
-                statement += `WHERE police_grid IN (${grid.join(", ")}) `;
+                var grids = req.query[key].split(',');
+                statement += `WHERE police_grid = '${grids[0]}'`;
+                if (grids.length > 1) {
+                    for(let x=1;x<grids.length;x++){
+                        if(x !== grids.length) {
+                            statement += ` OR `;
+                        }
+                        statement += `police_grid = '${grids[x]}'`;
+                    }
+                }
                 break;
             case 'neighborhood':
-                var neighborhood = req.query[key].split(',');
-                statement += `WHERE neighborhood_number IN (${neighborhood.join(", ")}) `;
+                var neighborhoods = req.query[key].split(',');
+                statement += `WHERE neighborhood_number = '${neighborhoods[0]}'`;
+                if(neighborhoods.length >1){
+                    for(let x=1;x<neighborhoods.length;x++){
+                        if(x !== neighborhoods.length){
+                            statement += ` OR `;
+                        }
+                        statement += `neighborhood_number = '${neighborhoods[x]}'`
+                    }
+                }
                 break;
             default:
                 var limit = 1000;
