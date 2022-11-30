10f0b753d5cf3b189b1e02ed1a0d171e0b90d0ec
Ani <rajani.lamichhane7@gmail.com>
2022-11-30 01:59:33 -0600

    fixxed get incidents

--- server.js
+++ server.js
@@ -86,50 +86,53 @@ app.get('/neighborhoods', (req, res) => {
 // GET request handler for crime incidents
 app.get('/incidents', (req, res) => {
 
-    var statement = 'SELECT case_number, strftime("%Y-%m-%d", date_time) as date, strftime("%H:%M:%S", date_time) as time, code, incident, police_grid, neighborhood_number, block FROM Incidents ';
+    var statement = 'SELECT case_number, strftime("%Y-%m-%d", date_time) as date, strftime("%H:%M:%S", date_time) as time, code, incident, police_grid, neighborhood_number, block FROM Incidents';
  
-    var query = Object.keys(req.query).filter(key => ['start_date', 'end_date', 'code', 'grid', 'neighborhood', 'limit'].includes(key));
-    if (!query.includes('limit')) {
-        query.push('limit')
+    var query = Object.keys(req.query).filter(key => ['start_date', 'end_date', 'code', 'grid', 'neighborhood'].includes(key));
+    if (query.length > 0) {
+        statement += ' WHERE ';
     }
-    query.forEach(key => {
+
+    var sql = query.map(key => {
         switch (key) {
             case 'start_date':
                 var start_date = req.query[key];
-                statement += `WHERE date >= (${start_date}) `;
-                break;
+                return `date >= '${start_date}'`
             case 'end_date':
                 var end_date = req.query[key];
-                statement += `WHERE date <= (${end_date}) `;
-                break;
+                return `date <= '${end_date}'`
             case 'code':
                 var code = req.query[key].split(',');
-                statement += `WHERE code IN (${code.join(", ")}) `;
-                break;
+                return `code IN (${code.join(", ")})`
             case 'grid':
                 var grid = req.query[key].split(',');
-                statement += `WHERE police_grid IN (${grid.join(", ")}) `;
-                break;
+                return `police_grid IN (${grid.join(", ")})`
             case 'neighborhood':
                 var neighborhood = req.query[key].split(',');
-                statement += `WHERE neighborhood_number IN (${neighborhood.join(", ")}) `;
-                break;
-            default:
-                var limit = 1000;
-                if (req.query['limit']) {
-                    limit = req.query['limit'];
-                }
-                statement += `ORDER BY date_time ASC LIMIT ${limit} `;
-                break;
+                return `neighborhood_number IN (${neighborhood.join(", ")})`
         }
-
     });
+    
+    statement += sql.join(' AND ');
+
+    var limit = 1000;
+    if (req.query['limit']) {
+        limit = req.query['limit'];
+    }
+
+    if (limit == 0) limit = 1000;
+    
+    statement += ` ORDER BY date_time ASC LIMIT ${limit} `;
 
     databaseSelect(statement, {})
     .then(rows => {
+        if (rows.length == 0) {
+            throw new Error('No rows found');
+        }
         res.status(200).type('json').send(rows);
     }).catch(error => {
-        res.status(401).type('json').send(error + ' you silly goose.');
+        console.log(error);
+        res.status(401).type('json').send('Unable to find the requested information you silly goose.');
     });
 });
 
@@ -207,6 +210,9 @@ function databaseRun(query, params) {
     })
 }
 
+app.get('*', function (req, res) {
+    res.status(404).type('json').send({ error: 404, message: `There was nothing found at ${req.url} you silly goose.`});
+});
 
 // Start server - listen for client connections
 app.listen(port, () => {
